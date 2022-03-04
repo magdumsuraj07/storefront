10a4aad6f25026a251a5ee77b9c749d7519aae04
magdumsuraj07 <59405552+magdumsuraj07@users.noreply.github.com>
2022-03-04 23:12:45 +0530

    Feature/order api (#3)
    * Rebase feature/Order-API to master
    
    * Implemented orders API
    
    * Implemented custom signals for decoupling apps.
    
    * Changes according to review Feature/order api

--- store/serializers.py
+++ store/serializers.py
@@ -1,6 +1,9 @@
 from decimal import Decimal
+from django.db import transaction
 from rest_framework import serializers
-from store.models import Cart, CartItem, Customer, Product, Collection, Review
+from .models import Cart, CartItem, Customer, Product, Collection, Review, \
+                    Order, OrderItem
+from .signals import order_created
 
 
 class CustomerSerializer(serializers.ModelSerializer):
@@ -14,8 +17,13 @@ class CustomerSerializer(serializers.ModelSerializer):
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'title', 'description', 'slug', 'inventory', 'unit_price', 'price_with_tax', 'collection']
-    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+        fields = [
+            'id', 'title', 'description', 'slug', 'inventory',
+            'unit_price', 'price_with_tax', 'collection'
+        ]
+    price_with_tax = serializers.SerializerMethodField(
+                                    method_name='calculate_tax'
+                                )
 
     def calculate_tax(self, product):
         return product.unit_price * Decimal(1.1)
@@ -32,7 +40,9 @@ class AddCartItemSerializer(serializers.ModelSerializer):
 
     def validate_product_id(self, value):
         if not Product.objects.filter(pk=value).exists():
-            raise serializers.ValidationError('No product with a given ID was found')
+            raise serializers.ValidationError(
+                                    'No product with a given ID was found'
+                              )
         else:
             return value
 
@@ -42,14 +52,20 @@ class AddCartItemSerializer(serializers.ModelSerializer):
         quantity = self.validated_data['quantity']
 
         try:
-            cart_item = CartItem.objects.get(cart_id=cart_id, product_id=product_id)
+            cart_item = CartItem.objects.get(
+                                            cart_id=cart_id,
+                                            product_id=product_id
+                                        )
             # Update an existing item
             cart_item.quantity = quantity
             cart_item.save()
             self.instance = cart_item
         except CartItem.DoesNotExist:
-            self.instance = CartItem.objects.create(cart_id=cart_id, **self.validated_data)
-        
+            self.instance = CartItem.objects.create(
+                                                cart_id=cart_id,
+                                                **self.validated_data
+                                            )
+
         return self.instance
 
     class Meta:
@@ -65,8 +81,10 @@ class UpdateCartItemSerializer(serializers.ModelSerializer):
 
 class CartItemSerializer(serializers.ModelSerializer):
     product = SimpleProductSerializer()
-    total_price = serializers.SerializerMethodField(method_name='get_total_price')
-    
+    total_price = serializers.SerializerMethodField(
+                                method_name='get_total_price'
+                             )
+
     class Meta:
         model = CartItem
         fields = ['id', 'product', 'quantity', 'total_price']
@@ -78,12 +96,14 @@ class CartItemSerializer(serializers.ModelSerializer):
 class CartSerializer(serializers.ModelSerializer):
     id = serializers.UUIDField(read_only=True)
     items = CartItemSerializer(many=True, read_only=True)
-    total_price = serializers.SerializerMethodField(method_name='get_total_price')
+    total_price = serializers.SerializerMethodField(
+                                    method_name='get_total_price'
+                             )
 
     class Meta:
         model = Cart
         fields = ['id', 'items', 'total_price']
-    
+
     def get_total_price(self, cart):
         total_price = 0
         for item in cart.items.all():
@@ -95,7 +115,7 @@ class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = ['id', 'date', 'name', 'description']
-    
+
     def create(self, validated_data):
         product_id = self.context['product_id']
         return Review.objects.create(product_id=product_id, **validated_data)
@@ -106,3 +126,65 @@ class CollectionSerializer(serializers.ModelSerializer):
         model = Collection
         fields = ['id', 'title', 'products_count']
     products_count = serializers.IntegerField(read_only=True)
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    product = SimpleProductSerializer()
+
+    class Meta:
+        model = OrderItem
+        fields = ['id', 'product', 'unit_price', 'quantity']
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True)
+
+    class Meta:
+        model = Order
+        fields = ['id', 'customer', 'placed_at', 'payment_status', 'items']
+
+
+class UpdateOrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ['payment_status']
+
+
+class CreateOrderSerializer(serializers.Serializer):
+    cart_id = serializers.UUIDField()
+
+    def validate_cart_id(self, cart_id):
+        if not Cart.objects.filter(pk=cart_id).exists():
+            raise serializers.ValidationError(
+                                'No cart with the given ID was found.'
+                            )
+        if CartItem.objects.filter(cart_id=cart_id).count() == 0:
+            raise serializers.ValidationError('The cart is empty.')
+        return cart_id
+
+    def save(self, **kwargs):
+        with transaction.atomic():
+            cart_id = self.validated_data['cart_id']
+
+            customer = Customer.objects.get(user_id=self.context['user_id'])
+            order = Order.objects.create(customer=customer)
+
+            cart_items = CartItem.objects \
+                                 .select_related('product') \
+                                 .filter(cart_id=cart_id)
+            order_items = [
+                OrderItem(
+                    order=order,
+                    product=item.product,
+                    quantity=item.quantity,
+                    unit_price=item.product.unit_price
+                ) for item in cart_items
+            ]
+            OrderItem.objects.bulk_create(order_items)
+
+            # Delete the cart after all operations are performed
+            Cart.objects.filter(pk=cart_id).delete()
+
+            order_created.send_robust(self.__class__, order=order)
+
+            return order
--- store/views.py
+++ store/views.py
@@ -2,22 +2,69 @@ from django.shortcuts import get_object_or_404
 from django.db.models.aggregates import Count
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, UpdateModelMixin
+from rest_framework.mixins import (
+    CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
+)
+
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
-
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from store.pagination import DefaultPagination
-from store.permissions import FullDjangoModelPermissions, IsAdminOrReadOnly, ViewCustomerHistoryPermission
+from store.permissions import IsAdminOrReadOnly, ViewCustomerHistoryPermission
 from .filters import ProductFilter
-from .models import Cart, CartItem, OrderItem, Product, Collection, Review, Customer
-from .serializers import AddCartItemSerializer, CartItemSerializer, CartSerializer, CollectionSerializer, CustomerSerializer, ProductSerializer, ReviewSerializer, UpdateCartItemSerializer
+from .models import (
+    Cart, CartItem, OrderItem, Product, Collection, Review, Customer, Order
+)
+
+from .serializers import (
+    AddCartItemSerializer, CartItemSerializer, CartSerializer,
+    CollectionSerializer, CreateOrderSerializer, CustomerSerializer,
+    ProductSerializer, ReviewSerializer, UpdateCartItemSerializer,
+    UpdateOrderSerializer, OrderSerializer
+)
+
+
+class OrderViewSet(ModelViewSet):
+    http_method_names = ['get', 'post', 'patch', 'delete', 'head', 'options']
+
+    def get_permissions(self):
+        if (self.request.method in ['PATCH', 'DELETE']):
+            return [IsAdminUser()]
+        return [IsAuthenticated()]
+
+    def create(self, request, *args, **kwargs):
+        serializer = CreateOrderSerializer(
+            data=request.data,
+            context={'user_id': self.request.user.id})
+        serializer.is_valid(raise_exception=True)
+        order = serializer.save()
+        serializer = OrderSerializer(order)
+        return Response(serializer.data)
+
+    def get_serializer_class(self):
+        if (self.request.method == 'POST'):
+            return CreateOrderSerializer
+        if (self.request.method == 'PATCH'):
+            return UpdateOrderSerializer
+        return OrderSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if (user.is_staff):
+            return Order.objects.all()
+
+        # We can directly use get and creating a customer is
+        # handled by 'create_customer_for_new_user' signal
+        customer_id = Customer.objects.only('id').get(user_id=user.id)
+        return Order.objects.filter(customer_id=customer_id)
 
 
 class CartItemViewSet(ModelViewSet):
     http_method_names = ['get', 'post', 'patch', 'delete']
+
     def get_serializer_class(self):
         if (self.request.method == 'POST'):
             return AddCartItemSerializer
@@ -30,9 +77,9 @@ class CartItemViewSet(ModelViewSet):
 
     def get_queryset(self):
         return CartItem.objects \
-                       .filter(cart_id = self.kwargs['cart_pk']) \
+                       .filter(cart_id=self.kwargs['cart_pk']) \
                        .select_related('product')
-    
+
 
 class CartViewSet(CreateModelMixin,
                   RetrieveModelMixin,
@@ -46,7 +93,7 @@ class ReviewViewSet(ModelViewSet):
     serializer_class = ReviewSerializer
 
     def get_queryset(self):
-        return Review.objects.filter(product_id = self.kwargs['product_pk'])
+        return Review.objects.filter(product_id=self.kwargs['product_pk'])
 
     def get_serializer_context(self):
         return {'product_id': self.kwargs['product_pk']}
@@ -72,16 +119,25 @@ class ProductViewSet(ModelViewSet):
 
 
 class CollectionViewSet(ModelViewSet):
-    queryset = Collection.objects.annotate(products_count=Count('products')).all()
+    queryset = Collection.objects \
+                         .annotate(products_count=Count('products')) \
+                         .all()
     serializer_class = CollectionSerializer
     permission_classes = [IsAdminOrReadOnly]
 
     def destroy(self, request, *args, **kwargs):
         collection = get_object_or_404(
-        Collection.objects.annotate(
-            products_count=Count('products')), pk=pk)
+                        Collection
+                        .objects
+                        .annotate(products_count=Count('products')), pk=pk
+                     )
         if collection.products.count() > 0:
-            return Response({'error': 'Collection cannot be deleted because it includes one or more products.'})
+            return Response(
+                        {
+                            'error': 'Collection cannot be deleted because \
+                                     it includes one or more products.'
+                        }
+                    )
         collection.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
@@ -95,9 +151,13 @@ class CustomerViewSet(ModelViewSet):
     def history(self, request, pk):
         return Response('ok')
 
-    @action(detail=False, methods=['GET', 'PUT'], permission_classes=[IsAuthenticated])
+    @action(
+        detail=False,
+        methods=['GET', 'PUT'],
+        permission_classes=[IsAuthenticated]
+    )
     def me(self, request):
-        (customer, created) = Customer.objects.get_or_create(user_id=request.user.id)
+        customer = Customer.objects.get(user_id=request.user.id)
         if (request.method == 'GET'):
             serializer = CustomerSerializer(customer)
             return Response(serializer.data)
