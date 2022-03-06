33718f6dfb038ab99d67557ced36ddf3affb94a2
Suraj Magdum <magdumsuraj07@gmail.com>
2022-03-06 13:15:54 +0530

    Added validation to image size

--- store/migrations/0014_productimage.py
+++ store/migrations/0014_productimage.py
@@ -7,16 +7,31 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('store', '0013_alter_orderitem_order'),
+        ("store", "0013_alter_orderitem_order"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='ProductImage',
+            name="ProductImage",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('image', models.ImageField(upload_to='store/images')),
-                ('product', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='images', to='store.product')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("image", models.ImageField(upload_to="store/images")),
+                (
+                    "product",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="images",
+                        to="store.product",
+                    ),
+                ),
             ],
         ),
     ]
